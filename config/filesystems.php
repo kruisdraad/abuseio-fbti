fa2d2b175e56bc00a3f308862a5768389d10427e
Bart Vrancken <bart@abuse.io>
2018-05-07 17:45:21 +0200

    update

---
+++ config/filesystems.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'default' => 'local',
+
+    'disks' => [
+
+        'local' => [
+            'driver' => 'local',
+            'root'   => storage_path(),
+        ],
+
+    ],
+
+];
---
+++ config/queue.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'default' => env('QUEUE_DRIVER', 'database'),
+
+    'connections' => [
+
+        'database' => [
+            'driver' => 'database',
+            'table' => 'jobs',
+            'queue' => 'default',
+            'retry_after' => 60,
+        ],
+
+    ],
+
+    'failed' => [
+        'database' => env('DB_CONNECTION', 'mysql'),
+        'table' => env('QUEUE_FAILED_TABLE', 'failed_jobs'),
+    ],
+
+];
